03e79c7b76a4b0a323c10089ea1c34b1b4677531
Luis Bonilla <luisbtcreative@gmail.com>
2023-06-20 10:03:32 -0500

    feat: add configuration to deploy app

---
+++ README.md
@@ -0,0 +1,42 @@
+# Backend poke
+## API REST Pokemon API
+
+## Features
+
+- ##### This project contains a mvc style architecture adapted to Nodejs. 
+- ##### It is developed with Typescript, nodejs, routing controller and mongo.
+- ##### As a package manager you have npm
+
+# Installation guide
+#### repository
+##### https://github.com/VladCode97/backend-poke
+- ###### in the terminal type npm install
+- ###### in the terminal type npm start-dev to run locally
+- ###### To run in prod, type npm run build and then npm start
+
+### At the deployment level, Azure was used, where the access url is
+- ##### https://waco-backend-test.scm.azurewebsites.net/
+
+### To access the swagger at this url is: 
+- ##### https://waco-backend-test.azurewebsites.net/swagger/#/
+
+-----------------
+
+# Frontend poke
+## Frontend Pokemon API
+
+## Features
+
+- ##### This project contains a hexagonal style architecture adapted to React. 
+- ##### It is developed with Typescript and react.
+- ##### As a package manager you have npm
+
+# Installation guide
+#### repository
+##### https://github.com/VladCode97/front-poke
+- ###### in the terminal type npm install
+- ###### in the terminal type npm start to run locally
+
+
+### At the deployment level, Azure was used, where the access url is
+- ##### https://front-backend.vercel.app/signIn
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,40 @@
-import {createExpressServer} from "routing-controllers";
+import { createExpressServer } from "routing-controllers";
 import "reflect-metadata";
-import {config} from "dotenv";
-import {UserView} from "./views/user.view";
-import {MongoSingleton} from "./database/mongo/config/mongo.config";
-import {AuthView} from "./views/auth.view";
-import {verifyRolesHandler} from "./utils/handler/verify-roles.handler";
-import {PokemonFavoritesView} from "./views/pokemon-favorites.view";
-import {yamlHandler} from "./utils/handler/yaml.handler";
-import {setup, serve} from "swagger-ui-express";
-import {NextFunction, Request, Response} from "express";
+import { config } from "dotenv";
+import { UserView } from "./views/user.view";
+import { MongoSingleton } from "./database/mongo/config/mongo.config";
+import { AuthView } from "./views/auth.view";
+import { verifyRolesHandler } from "./utils/handler/verify-roles.handler";
+import { PokemonFavoritesView } from "./views/pokemon-favorites.view";
+import { yamlHandler } from "./utils/handler/yaml.handler";
+import { setup, serve } from "swagger-ui-express";
+import { NextFunction, Request, Response } from "express";
 
 config();
 
-async function main() {
-    await yamlHandler();
-    await MongoSingleton.Instance.establishConnection();
-    const server = createExpressServer({
-        controllers: [UserView, AuthView, PokemonFavoritesView],
-        classTransformer: false,
-        authorizationChecker: verifyRolesHandler,
-        validation: {
-            always: true
-        },
-        cors: "*",
-    });
-    server.use("/swagger", async (request: Request, response: Response, next: NextFunction) => {
-        next()
-    }, serve, setup(await yamlHandler()))
-    server.listen(Number(process.env.PORT), () =>
-        console.log(`http://localhost:${Number(process.env.PORT)}`)
-    );
+export async function main() {
+  await yamlHandler();
+  await MongoSingleton.Instance.establishConnection();
+  const server = createExpressServer({
+    controllers: [UserView, AuthView, PokemonFavoritesView],
+    classTransformer: false,
+    authorizationChecker: verifyRolesHandler,
+    validation: {
+      always: true,
+    },
+    cors: "*",
+  });
+  server.use(
+    "/swagger",
+    async (request: Request, response: Response, next: NextFunction) => {
+      next();
+    },
+    serve,
+    setup(await yamlHandler())
+  );
+  server.listen(Number(process.env.PORT), () =>
+    console.log(`http://localhost:${Number(process.env.PORT)}`)
+  );
 }
 
 main();
